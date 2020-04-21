aa0ef6ccccb062163f0acb96e251aacc0d08a137
heungjin-bae <pdjins00@gmail.com>
2020-04-21 12:01:02 +0900

    errer

--- index.js
+++ index.js
@@ -37,8 +37,10 @@ app.use('/contacts',require('./routes/contacts'));
 app.use((req, res, next) =>
     res.status(404).send('Sorry cant find that!'));
 
-app.use((err, req, res, next) =>
-    res.status(500).send('Something broke!'));
+app.use(function(err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+});
 
 // Port setting
 var port = 8000;
